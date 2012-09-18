6b7d72f077caae4630b97a25e8dd99bb5d570ad9
Gavin Mogan <gavin@qudos.com>
2012-09-18 13:38:47 -0700

    upgrade to support chef 10.14

--- libraries/provider.rb
+++ libraries/provider.rb
@@ -1,25 +1,14 @@
 class RvmDeployProvider < Chef::Provider::Deploy::Revision
-  def deploy
-    enforce_ownership
-    verify_directories_exist
-    update_cached_repo
-    copy_cached_repo
+  def symlink
+    precompile_assets
+    super
+  end
+
+  def release_created(release_path)
+    super(release_path)
     setup_load_paths
     create_rvmrc
     create_gemset
-    install_gems
-    enforce_ownership
-    callback(:before_migrate, @new_resource.before_migrate)
-    migrate
-    precompile_assets
-    callback(:before_symlink, @new_resource.before_symlink)
-    symlink
-    callback(:before_restart, @new_resource.before_restart)
-    restart
-    notify_airbrake
-    callback(:after_restart, @new_resource.after_restart)
-    cleanup!
-    Chef::Log.info "#@new_resource deployed to #{@new_resource.deploy_to}"
   end
 
   def setup_load_paths
@@ -61,13 +50,44 @@ class RvmDeployProvider < Chef::Provider::Deploy::Revision
   end
 
   def install_gems
-    rvm_shell "install gems" do
-      ruby_string new_resource.ruby_string
-      cwd release_path
-      user new_resource.user
-      code "bundle install"
-      action :nothing
-    end.run_action(:run)
+    Chef::Log.info "Running bundle install"
+    converge_by("Running bundle install") do
+      directory "#{new_resource.shared_path}/vendor_bundle" do
+        owner new_resource.user
+        group new_resource.group
+        mode '0755'
+        action :nothing
+      end.run_action(:create)
+      directory "#{release_path}/vendor" do
+        owner new_resource.user
+        group new_resource.group
+        mode '0755'
+        action :nothing
+      end.run_action(:create)
+      link "#{release_path}/vendor/bundle" do
+        to "#{new_resource.shared_path}/vendor_bundle"
+        action :nothing
+      end.run_action(:create)
+      common_groups = %w{development test cucumber staging production}
+      # common_groups -= [new_resource.environment_name]
+      # FIXME
+      common_groups -= ["production"]
+      common_groups = common_groups.join(' ')
+
+      # Check for a Gemfile.lock
+      bundler_deployment = ::File.exists?(::File.join(release_path, "Gemfile.lock"))
+       
+      Chef::Log.info "bundle install --path=vendor/bundle #{bundler_deployment ? "--deployment " : ""}--without #{common_groups}"
+
+      rvm_shell "install gems" do
+        ruby_string new_resource.ruby_string
+        cwd release_path
+        user new_resource.user
+        environment new_resource.environment
+        code "bundle install --path=vendor/bundle #{bundler_deployment ? "--deployment " : ""}--without #{common_groups}"
+        action :nothing
+      end.run_action(:run)
+    end
   end
 
   # @see http://jessewolgamott.com/blog/2012/09/03/the-one-where-you-take-your-deploy-to-11-asset-pipeline/
@@ -86,14 +106,16 @@ class RvmDeployProvider < Chef::Provider::Deploy::Revision
 
   def precompile_assets
     if @new_resource.precompile_assets # && paths_changed?(%w(app/assets lib/assets vendor/assets Gemfile.lock config/application.rb))
-      rvm_shell "precompile assets" do
-        ruby_string new_resource.ruby_string
-        cwd release_path
-        user new_resource.user
-        code "bundle exec rake assets:precompile"
-        environment new_resource.environment
-        action :nothing
-      end.run_action(:run)
+      converge_by("precompiling assets") do
+        rvm_shell "precompile assets" do
+          ruby_string new_resource.ruby_string
+          cwd release_path
+          user new_resource.user
+          code "RAILS_ENV=production bundle exec rake assets:precompile"
+          environment new_resource.environment
+          action :nothing
+        end.run_action(:run)
+      end
     end
   end
 
@@ -101,16 +123,18 @@ class RvmDeployProvider < Chef::Provider::Deploy::Revision
     run_symlinks_before_migrate
 
     if @new_resource.migrate
-      enforce_ownership
+      converge_by("run migrate database ") do
+        enforce_ownership
 
-      rvm_shell "migrate database" do
-        ruby_string new_resource.ruby_string
-        cwd release_path
-        user new_resource.user
-        code new_resource.migration_command
-        environment new_resource.environment
-        action :nothing
-      end.run_action(:run)
+        rvm_shell "migrate database" do
+          ruby_string new_resource.ruby_string
+          cwd release_path
+          user new_resource.user
+          code new_resource.migration_command
+          environment new_resource.environment
+          action :nothing
+        end.run_action(:run)
+      end
     end
   end
 
@@ -127,4 +151,5 @@ class RvmDeployProvider < Chef::Provider::Deploy::Revision
       action :nothing
     end.run_action(:run)
   end
+
 end
